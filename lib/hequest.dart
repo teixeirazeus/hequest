e82b10744bb7d1fc01288c0a767106eff21ec31b
teixeirazeus <teixeira.zeus@gmail.com>
2022-09-01 18:23:40 -0300

    first commit

---
+++ lib/hequest.dart
@@ -0,0 +1,96 @@
+// Copyright 2021 DevHops. All rights reserved.
+library hequest;
+
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:http/http.dart';
+
+class Hequest {
+  String baseUrl;
+  Hequest({
+    required this.baseUrl,
+  });
+
+  Future<Response> get(String route) async {
+    return await http.get(Uri.parse(baseUrl + route));
+  }
+
+  Future<Response> getWithAuth(String route, String token) async {
+    return await http.get(
+      Uri.parse(baseUrl + route),
+      headers: <String, String>{
+        'Authorization': 'Bearer $token',
+      },
+    );
+  }
+
+  Future<Response> post(String route, Map<dynamic, dynamic> jsonMap) async {
+    return await http.post(
+      Uri.parse(baseUrl + route),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(jsonMap),
+    );
+  }
+
+  Future<Response> postWithAuth(
+      String route, String token, Map<dynamic, dynamic> jsonMap) async {
+    return await http.post(
+      Uri.parse(baseUrl + route),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+        'Authorization': 'Bearer $token',
+      },
+      body: jsonEncode(jsonMap),
+    );
+  }
+
+  Future<Response> put(String route, Map<dynamic, dynamic> jsonMap) async {
+    return await http.put(
+      Uri.parse(baseUrl + route),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8'
+      },
+      body: jsonEncode(jsonMap),
+    );
+  }
+
+  Future<Response> putWithAuth(
+      String route, String token, Map<dynamic, dynamic> jsonMap) async {
+    return await http.put(
+      Uri.parse(baseUrl + route),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+        'Authorization': 'Bearer $token',
+      },
+      body: jsonEncode(jsonMap),
+    );
+  }
+
+  Future<Response> deleteWithAuth(String route, String token) async {
+    return await http.delete(
+      Uri.parse(baseUrl + route),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+        'Authorization': 'Bearer $token',
+      },
+    );
+  }
+
+  Future<Response> delete(String route) async {
+    return await http.delete(Uri.parse(baseUrl + route));
+  }
+
+  Future<Response> deleteWithAuthNBody(
+      String route, String token, Map<String, dynamic> jsonMap) async {
+    return await http.delete(
+      Uri.parse(baseUrl + route),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+        'Authorization': 'Bearer $token',
+      },
+      body: jsonEncode(jsonMap),
+    );
+  }
+}
